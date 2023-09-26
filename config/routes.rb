9f10b3e135dd38cad68469f795c68625e0df25ed
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-26 15:47:37 -0400

    add route and controller for category

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'categories/new'
+  get 'categories/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/add_category', to: 'categories#new', as: 'new_category'
+  post '/add_category', to: 'categories#create', as: 'create_category'
   # Defines the root path route ("/")
   # root "articles#index"
 end
