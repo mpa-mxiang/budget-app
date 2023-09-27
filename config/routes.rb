f43def623dbd064fa35b03f7412e2dc17b11f43b
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-26 23:23:36 -0400

    create home and transactions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'transactions/index'
+  get 'transactions/new'
+  get 'transactions/create'
+  get 'home/index'
   get 'categories/new'
   get 'categories/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/add_category', to: 'categories#new', as: 'new_category'
+  get '/add_category', to: 'categories#new', as: 'add_category'
   post '/add_category', to: 'categories#create', as: 'create_category'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+  resources :transactions
+  get '/add_transaction', to: 'transactions#new', as: 'add_transaction'
 end
