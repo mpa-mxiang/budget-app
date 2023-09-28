2f57a7d4dcdb2e96f8df546065acf28fdabf2c07
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-28 14:37:32 -0400

    add route and view for category

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  get 'transactions/index'
-  get 'transactions/new'
-  get 'transactions/create'
-  get 'home/index'
-  get 'categories/new'
-  get 'categories/create'
-  get '/add_category', to: 'categories#new', as: 'add_category'
-  post '/add_category', to: 'categories#create', as: 'create_category'
-  root 'home#index'
-  resources :transactions
-  get '/add_transaction', to: 'transactions#new', as: 'add_transaction'
+  resources :groups, only: [:new, :create]
 end
