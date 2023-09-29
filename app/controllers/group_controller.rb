d047fbb106fddb0d9f07faf7e8cc41506e27a687
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-09-28 21:30:58 -0400

    add methods for group controller

---
+++ app/controllers/group_controller.rb
@@ -0,0 +1,23 @@
+class GroupController < ApplicationController
+    def new
+        @group = Group.new
+    end
+    def index
+        @group = Group.all
+      end
+    def create
+        @group = Group.new(category_params)
+    
+        if @group.save
+          redirect_to categories_path, notice: 'Category was successfully created.'
+        else
+          render :new
+        end
+      end
+    
+      private
+    
+      def category_params
+        params.require(:group).permit(:name, :icon, :other_attributes)
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :groups, only: [:new, :create]
+  resources :group, only: [:new, :create]
 end
